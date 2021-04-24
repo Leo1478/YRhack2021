d14cc2e31ac4e40ba1f5ff2c168764f4fc9a005b
Meow157 <57512426+MiaoE@users.noreply.github.com>
2021-04-24 06:50:02 -0400

    Create detect.js

---
+++ detect.js
@@ -0,0 +1,8 @@
+//cuz idk how chrome extension works, but might as well do something
+function detectHere() {
+    var element = document.getElementsByClassName("oIy2qc");
+    var length = element.length;
+    var latest = element[length - 1].toLowerCase();
+    return (latest == "here");
+}
+//what am i doing
